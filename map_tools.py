0624e1ed1d950d4fa50360aee9b25e379251a45f
Valeriy.Burlaka <chebusun@gmail.com>
2014-02-17 15:33:12 +0200

    Moved map_overview parsing from Map class to new MapParser class;

--- map_tools.py
+++ map_tools.py
@@ -7,6 +7,69 @@ import logging
 from math import sqrt
 
 
+class MapParser:
+    """
+    Component, responsible for parsing map data from
+    a given map_overview file.
+    Processes HTML file (as string) and extracts JSON data
+    with all sectors/villages from it.
+    Manipulates extracted JSON data and structures it to
+    bring it to view [sector_data1, sector_data2, ...], where
+    sector_data is a dictionary:
+    {key=tuple(x_coordinate, y_coordinate): value=[village_data], ..}
+
+    Methods:
+
+    collect_sector_data(overview_html):
+        returns structured sectors_data
+    """
+
+    def collect_sector_data(self, map_overview_html):
+        structured_sectors = []
+        raw_sectors_data = self.get_map_data(map_overview_html)
+        for sector_data in raw_sectors_data:
+            sector = {}
+            sector_x = sector_data['x']
+            sector_y = sector_data['y']
+            sector_villages = sector_data['data']['villages']
+            # Some fun below: there are cases, when game stores
+            # sector villages in different data types.
+            # 1) case: index is x coordinate, value is a dict of villages
+            # that lie on a y axis for given x.
+            if isinstance(sector_villages, list):
+                sector_gen = enumerate(sector_villages)
+            # 2) case: key is x coordinate, value is a dict of villages
+            # which lie on a y axis for given x.
+            elif isinstance(sector_villages, dict):
+                sector_gen = sector_villages.items()
+            assert sector_gen
+
+            for x, y_axis in sector_gen:
+                x = int(x)
+                for y, village_data in y_axis.items():
+                    villa_x = sector_x + x
+                    villa_y = sector_y + int(y)
+                    villa_coords = (villa_x, villa_y)
+                    sector[villa_coords] = village_data
+            structured_sectors.append(sector)
+
+        return structured_sectors
+
+    @staticmethod
+    def get_map_data(html_data):
+        """
+        Looks for string containg sector data (JSON)
+        Returns unstructured dict containing sector data
+        """
+        ptrn = re.compile('TWMap.sectorPrefech = ([\W\w]+?\]);')
+        match = re.search(ptrn, html_data)
+        js_res = match.group(1)
+        res = json.loads(js_res)
+        return res
+
+
+
+
 class Map:
     """
     A collecton of barbarian & bonus Villages (neutral).
@@ -69,38 +132,7 @@ class Map:
         logging.info(event_msg)
         map_html = self.get_map_overview(x, y)
         # list of dicts, each dict represents 1 sector
-        sectors = self.get_map_data(map_html)
-        for sector in sectors:
-            sector_x = sector['x']
-            sector_y = sector['y']
-            # hold coords of villages found, need to determine sector's corners
-            sector_coords = []
-            sector_villages = sector['data']['villages']
-            # Some fun below: there are cases, when game stores
-            # sector villages in different data types.
-            # 1) case:
-            # index is x coordinate, value is a dict of villages
-            # which lie on a y axis for given x.
-            if isinstance(sector_villages, list):
-                sector_gen = enumerate(sector_villages)
-            # key is x coordinate, value is a dict of villages
-            # which lie on a y axis for given x.
-            elif isinstance(sector_villages, dict):
-                sector_gen = sector_villages.items()
 
-            for x, y_axis in sector_gen:
-                x = int(x)
-                for y, village_data in y_axis.items():
-                    # check if village is Bonus/Barbarian
-                    if self.is_valid(village_data):
-                        villa_x = sector_x + x
-                        villa_y = sector_y + int(y)
-                        villa_coords = (villa_x, villa_y)
-                        sector_coords.append(villa_coords)
-                        if not villa_coords in self.villages:
-                            village = self.get_village(villa_coords,
-                                                       village_data)
-                            self.villages[villa_coords] = village
 
             if depth:
                 sector_corners = self.get_sector_corners(sector_coords)
@@ -176,14 +208,7 @@ class Map:
         distance = sqrt(side_x**2 + side_y**2)
         return round(distance, 2)
 
-    def get_map_data(self, html_data):
-        """Returns dict containing sector data"""
-        # Looking for string containg sector data (JSON)
-        ptrn = re.compile('TWMap.sectorPrefech = ([\W\w]+?\]);')
-        match = re.search(ptrn, html_data)
-        js_res = match.group(1) # json string
-        res = json.loads(js_res)
-        return res
+
 
     def get_targets_in_radius(self, radius, source_coords):
         targets = []
